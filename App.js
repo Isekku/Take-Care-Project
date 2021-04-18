87a4ebf3808d903f34369be3a541b651745060b3
Oryp574 <72729853+Oryp574@users.noreply.github.com>
2021-04-18 17:12:58 +0200

    Update App.js
    Il y aura surement une erreur quand on va essayer de run le projet, donc avant d'avoir ce problème il faut import axios, "npm install axios"

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import axios from 'axios'
 
+
 import SignInScreen from './components/SignInScreen';
 import SignUpChoose from './components/SignUpChoose';
 import DropScreen from './components/DropScreen';
