df6314de1aad44d2737905f486a3e5ff53768845
Sékou <Sekoucisse1701@gmail.com>
2021-04-20 14:30:09 +0200

    Ajout d'axios dans package.json + ajout de home

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import axios from 'axios'
+import axios from 'axios';
 
 
 import SignInScreen from './components/SignInScreen';
 import SignUpChoose from './components/SignUpChoose';
 import DropScreen from './components/DropScreen';
-import SignUpScreen_Med_one from './components/SignUpScreen_Med_one';
-import SignUpScreen_Med_two from './components/SignUpScreen_Med_two';
-import SignUpScreen_Med_three from './components/SignUpScreen_Med_three';
 import HomeScreen from './components/HomeScreen';
 import SignUpSrcollview from './components/SignUpScrollview';
-import SignUpMedScrollview from './components/SignUpScrollMedview'
+import SignUpMedScrollview from './components/SignUpScrollMedview';
+import SettingScreen from './components/SettingScreen';
+import AvatarScreen from './components/AvatarScreen';
 
 
 const Stack = createStackNavigator();
@@ -32,6 +31,8 @@ export default function App() {
 
       <Stack.Screen name="Connexion" component={SignInScreen} />
       <Stack.Screen name="Accueil" component={HomeScreen} />
+      <Stack.Screen name="Parametre" component={SettingScreen} />
+      <Stack.Screen name="Profil" component={AvatarScreen} />
       <Stack.Screen name="Choix Inscription" component={SignUpChoose} />
       <Stack.Screen name="Inscription scroll" component={SignUpSrcollview} />
       <Stack.Screen name="Inscription Med scroll" component={SignUpMedScrollview} />
